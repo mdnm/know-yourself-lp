cbec845582e9441495501d375a7f5ba52821c501
Mateus De Nardo <m.nardo@epilot.cloud>
2023-01-24 00:03:22 +0100

    feat: boostrap basic lp

---
+++ pages/api/subscribe.ts
@@ -0,0 +1,42 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+const API_KEY = process.env.CONVERTKIT_API_KEY;
+const BASE_URL = "https://api.convertkit.com/v3";
+
+function subscribeToForm(params: {
+  formId: string;
+  name: string;
+  email: string;
+}) {
+  const url = [BASE_URL, `forms`, params.formId, "subscribe"].join("/");
+
+  const headers = new Headers({
+    "Content-Type": "application/json; charset=utf-8",
+  });
+
+  return fetch(url, {
+    method: "POST",
+    headers,
+    body: JSON.stringify({
+      api_key: API_KEY,
+      email: params.email,
+    }),
+  });
+}
+
+async function subscribeToFormHandler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const body = req.body;
+
+  await subscribeToForm({
+    formId: body.formId,
+    name: body.name,
+    email: body.email,
+  });
+
+  return res.send({ success: true });
+}
+
+export default subscribeToFormHandler;
---
+++ pages/components/ConvertkitSignupForm.tsx
@@ -0,0 +1,95 @@
+import { FormEventHandler, useCallback, useState } from "react";
+
+const ConvertkitSignupForm: React.FC<
+  React.PropsWithChildren<{
+    formId: string;
+  }>
+> = ({ formId, children }) => {
+  const formData = {
+    name: "name",
+    email: "email",
+  };
+  const [success, setSuccess] = useState<boolean | undefined>();
+
+  const onSubmit: FormEventHandler = useCallback(
+    async (event) => {
+      event.preventDefault();
+
+      const target = event.target as HTMLFormElement;
+      const data = new FormData(target);
+      const name = data.get(formData.name);
+      const email = data.get(formData.email);
+
+      const body = JSON.stringify({
+        formId,
+        name,
+        email,
+      });
+
+      const headers = new Headers({
+        "Content-Type": "application/json; charset=utf-8",
+      });
+
+      try {
+        await fetch(`/api/subscribe`, {
+          method: "POST",
+          mode: "cors",
+          cache: "no-cache",
+          headers,
+          body,
+        });
+
+        setSuccess(true);
+      } catch {
+        setSuccess(false);
+      }
+    },
+    [formId]
+  );
+
+  if (success === false) {
+    return <p>Apologies, an error occurred</p>;
+  }
+
+  if (success) {
+    return (
+      <p>
+        Check out your email in some minutes! (if that doesn&apos;t work, try
+        opening the Spam folder)
+      </p>
+    );
+  }
+
+  return (
+    <>
+      <form
+        target="_blank"
+        className={`space-around flex flex-col w-full justify-center gap-4 max-w-screen-sm mx-auto`}
+        onSubmit={onSubmit}
+      >
+        <input
+          type="text"
+          className="w-full rounded-md px-3 py-2 text-base md:text-lg text-black"
+          name={formData.name}
+          aria-label="Name"
+          placeholder="Your name"
+          required
+        />
+        <input
+          type="email"
+          className="w-full rounded-md px-3 py-2 text-base md:text-lg text-black"
+          name={formData.email}
+          aria-label="Email"
+          placeholder="your@email.com"
+          required
+        />
+
+        <button className="bg-blue-600 min-w-[6rem] rounded-md text-lg md:text-xl font-bold py-2">
+          {children ?? "Sign up"}
+        </button>
+      </form>
+    </>
+  );
+};
+
+export default ConvertkitSignupForm;
