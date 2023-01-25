d531078bfb3fd8876efe6deedf43ff6f90bad1fa
Mateus De Nardo <m.nardo@epilot.cloud>
2023-01-25 17:28:31 +0100

    chore: add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,30 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Script from "next/script";
 import { Toaster } from "react-hot-toast";
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
       <Component {...pageProps} />;
       <Toaster />
     </>
